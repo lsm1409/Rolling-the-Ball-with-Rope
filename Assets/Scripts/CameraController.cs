c912171b56a64195ff677ee90d13a52dc28562d0
Sangmin <lsm1409@naver.com>
2019-02-26 16:25:49 +0900

    Camera Rig 제거

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,16 +1,5 @@
 ﻿using UnityEngine;
 
-/// <summary>
-/// Camera controller.
-/// Camera Rig 오브젝트에 적용
-/// 
-/// Camera Rig (부모)
-///   - Main Camera (자식) * 필수요소
-/// 
-/// Main Camera가 달린 Camera Rig를 공 위치로 이동시키고 카메라의 위치를 조작하는 스크립트
-/// </summary>
-/// 
-/// 
 public class CameraController : MonoBehaviour
 {
     public GameObject Player;
@@ -24,7 +13,6 @@ public class CameraController : MonoBehaviour
     private Vector3[] cameraReversePositions = { new Vector3(-4, 6, -20), new Vector3(-8, 2, 0), new Vector3(8, 6, 20), new Vector3(8, 2, 0) };
     private Vector3[] cameraRotations = { new Vector3(0, 0, 0), new Vector3(0, 90, 0), new Vector3(0, 180, 0), new Vector3(0, 270, 0) };
 
-
     private void Start()
     {
         offsetNum = 0;
@@ -35,14 +23,14 @@ public class CameraController : MonoBehaviour
     {
         if (!UIController.GameOver)
         {
-            Camera.main.transform.position = isForward
-                ? Vector3.Lerp(Camera.main.transform.position, Player.transform.position + cameraPositions[offsetNum], Time.deltaTime * CameraSpeed)
-                : Vector3.Lerp(Camera.main.transform.position, Player.transform.position + cameraReversePositions[offsetNum], Time.deltaTime * CameraSpeed);
-            Camera.main.transform.rotation = Quaternion.Lerp(Camera.main.transform.rotation, Quaternion.Euler(cameraRotations[offsetNum]), Time.deltaTime * CameraSpeed);
+            transform.position = isForward
+                ? Vector3.Lerp(transform.position, Player.transform.position + cameraPositions[offsetNum], Time.deltaTime * CameraSpeed)
+                : Vector3.Lerp(transform.position, Player.transform.position + cameraReversePositions[offsetNum], Time.deltaTime * CameraSpeed);
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(cameraRotations[offsetNum]), Time.deltaTime * CameraSpeed);
         }
         else
         {
-            Camera.main.transform.RotateAround(Player.transform.position, Vector3.up, 90 * Time.deltaTime);
+            transform.RotateAround(Player.transform.position, Vector3.up, 90 * Time.deltaTime);
         }
 
         // 공의 속도가 빨라지면 카메라의 viewing field를 확장합니다.
