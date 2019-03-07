839e292471d5f9ca06c483af9fec7e1019260fa0
JINHYEOK-YOO <yujh703@gmail.com>
2019-03-07 12:02:34 +0900

    0번 맵 구성 완료 (#2)
    * init
    
    * Coin 프리팹 추가
    
    * 블럭이랑 태그 일부 생성
    
    * Bonus Coin 프리팹 추가
    
    * Revert "Bonus Coin 프리팹 추가"
    
    This reverts commit bda75b9356dedef1ced74ba11769db38d5dea70e.
    
    * Revert "Revert "Bonus Coin 프리팹 추가""
    
    This reverts commit 5fd060e636aa5c65d586de0927d951cf0732dd6e.
    
    * Coin, Flag 모델, 프리팹 추가
    
    * 맵을 일부 구성
    
    * 방향키 조작 추가, 최고속도 하향, Ground 프리팹 머티리얼 적용
    
    * AppController -> AppDirector 이름 변경
    
    * 맵 완성 및 엘리베이터 스크립트 추가
    
    * 블럭 수정
    
    * 0번 스테이지 배경 맵 실험적용

---
+++ Assets/Scripts/CoinController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Coin controller.
+/// 게임 속 Coin을 y축 회전시키는 스크립트
+/// </summary>
+/// 
+/// 
+public class CoinController : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(Time.deltaTime * 45, 0, 0);
+    }
+}
