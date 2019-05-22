05d1db786a3b5bf079b247e15e9255d701ebc86a
carlyzzz <carlyzzz@naver.com>
2019-05-23 00:02:48 +0900

    씬, 음악(미완)
    씬연결, 음악 설정

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+            DontDestroyOnLoad(transform.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "SelectStage")
+        {
+            DontDestroyOnLoad(transform.gameObject);
+            //Destroy(transform.gameObject);
+        }
+        else if (SceneManager.GetActiveScene().name == "Title")
+        {
+            DontDestroyOnLoad(transform.gameObject);
+            //Destroy(transform.gameObject);
+        }
+        else
+        {
+            Destroy(transform.gameObject);
+        }
+    }
+}
