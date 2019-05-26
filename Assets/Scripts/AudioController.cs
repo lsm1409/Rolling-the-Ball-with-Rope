b8c4dc34dd9015d94f72a6264f0b08b9781497df
Yoo Jinhyeok <yujh703@gmail.com>
2019-05-27 00:42:25 +0900

    타이틀의 Sound On/Off 구현

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -1,11 +1,34 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class AudioController : MonoBehaviour
 {
+    public Sprite soundOn;
+    public Sprite soundOff;
+
+    void Start()
+    {
+        if (!AudioListener.pause)
+        {
+            GetComponent<Image>().sprite = soundOn;
+        }
+        else if (AudioListener.pause)
+        {
+            GetComponent<Image>().sprite = soundOff;
+        }
+    }
+
     public void SoundOnOff()
     {
-        AudioListener.pause = !AudioListener.pause;
+        if(AudioListener.pause)
+        {
+            AudioListener.pause = false;
+            GetComponent<Image>().sprite = soundOn;
+        }
+        else if (!AudioListener.pause)
+        {
+            AudioListener.pause = true;
+            GetComponent<Image>().sprite = soundOff;
+        }
     }
 }
