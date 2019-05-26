ef532eb6ac9ddc76015e27008f368f8abde48eff
Yoo Jinhyeok <yujh703@gmail.com>
2019-05-27 00:32:04 +0900

    스크립트 및 오브젝트 정리

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -1,40 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class AudioController : MonoBehaviour
 {
-    static bool isFirst = true;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        if (isFirst)
-        {
-            this.GetComponent<AudioSource>().Play();
-            isFirst = false;
-        }
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (SceneManager.GetActiveScene().name == "SelectStage")
-        {
-            DontDestroyOnLoad(transform.gameObject);
-        }
-        else if (SceneManager.GetActiveScene().name == "Title")
-        {
-            DontDestroyOnLoad(transform.gameObject);
-        }
-        else
-        {
-            Destroy(transform.gameObject);
-        }
-    }
-
     public void SoundOnOff()
     {
         AudioListener.pause = !AudioListener.pause;
---
+++ Assets/Scripts/SceneDirector.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneDirector : MonoBehaviour
+{
+    public void TitleScene()
+    {
+        Destroy(GameObject.Find("Button Sound"), 0.5f);
+        SceneManager.LoadScene("Title");
+    }
+
+    public void StageSelect()
+    {
+        Destroy(GameObject.Find("Button Sound"), 0.5f);
+        SceneManager.LoadScene("SelectStage");
+    }
+
+    public void PlayStage0()
+    {
+        Destroy(GameObject.Find("Button Sound"), 0.5f);
+        SceneManager.LoadScene("Stage#0");
+    }
+}
