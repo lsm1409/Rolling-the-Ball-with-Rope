6ad82128669b4cca7bfaa4e6888c200878d49569
carlyzzz <carlyzzz@naver.com>
2019-10-22 12:45:31 +0900

    스테이지셀렉트
    락됬을경우 스테이지로 안넘어가게

--- Assets/Scripts/StageSelectController.cs
+++ Assets/Scripts/StageSelectController.cs
@@ -11,14 +11,19 @@ public class StageSelectController : MonoBehaviour
     string line, temp;
     static int[,] record = new int[3, 4]; //순서대로 클리어 여부 별의 갯수와 시간 no클리어 0 클리어 1
     int cnt;
+    public Image st0_star1;
+    public Image st0_star2;
+    public Image st0_star3;
     public Image st1_star1;
     public Image st1_star2;
     public Image st1_star3;
     public Image st2_star1;
     public Image st2_star2;
     public Image st2_star3;
+    public Image st0_lock;
     public Image st1_lock;
     public Image st2_lock;
+    public Text st0_time;
     public Text st1_time;
     public Text st2_time;
 
@@ -37,18 +42,36 @@ public class StageSelectController : MonoBehaviour
             cnt++;
         }
         file.Close();
+        if (record[0, 1] == 1) st1_lock.gameObject.SetActive(false);
         if (record[1, 1] == 1) st2_lock.gameObject.SetActive(false);
-        if (record[0, 2] >= 1) st1_star1.gameObject.SetActive(true);
-        if (record[0, 2] >= 2) st1_star2.gameObject.SetActive(true);
-        if (record[0, 2] == 3) st1_star3.gameObject.SetActive(true);
-        if (record[1, 2] >= 1) st2_star1.gameObject.SetActive(true);
-        if (record[1, 2] >= 2) st2_star2.gameObject.SetActive(true);
-        if (record[1, 2] == 3) st2_star3.gameObject.SetActive(true);
-        if(record[0, 3] > 0)
+        if (record[0, 2] >= 1) st0_star1.gameObject.SetActive(true);
+        if (record[0, 2] >= 2) st0_star2.gameObject.SetActive(true);
+        if (record[0, 2] == 3) st0_star3.gameObject.SetActive(true);
+        if (record[1, 2] >= 1) st1_star1.gameObject.SetActive(true);
+        if (record[1, 2] >= 2) st1_star2.gameObject.SetActive(true);
+        if (record[1, 2] == 3) st1_star3.gameObject.SetActive(true);
+        if (record[2, 2] >= 1) st2_star1.gameObject.SetActive(true);
+        if (record[2, 2] >= 2) st2_star2.gameObject.SetActive(true);
+        if (record[2, 2] == 3) st2_star3.gameObject.SetActive(true);
+        if (record[0, 3] > 0)
         {
             int min = 0, sec = 0;
             min = record[0, 3] / 60;
             sec = record[0, 3] % 60;
+            if (min < 10 && sec < 10)
+                st0_time.text = "0" + min + " : 0" + sec;
+            else if (min >= 10 && sec < 10)
+                st0_time.text = min + " : 0" + sec;
+            else if (min < 10 && sec >= 10)
+                st0_time.text = "0" + min + " : " + sec;
+            else
+                st0_time.text = min + " : " + sec;
+        }
+        if (record[1, 3] > 0)
+        {
+            int min = 0, sec = 0;
+            min = record[1, 3] / 60;
+            sec = record[1, 3] % 60;
             if (min < 10 && sec < 10)
                 st1_time.text = "0" + min + " : 0" + sec;
             else if (min >= 10 && sec < 10)
@@ -58,7 +81,7 @@ public class StageSelectController : MonoBehaviour
             else
                 st1_time.text = min + " : " + sec;
         }
-        if (record[1, 3] > 0)
+        if (record[2, 3] > 0)
         {
             int min = 0, sec = 0;
             min = record[1, 3] / 60;
@@ -82,10 +105,17 @@ public class StageSelectController : MonoBehaviour
             case 0:
                 transform.Find("Stage0").gameObject.SetActive(true);
                 transform.Find("Stage1").gameObject.SetActive(false);
+                transform.Find("Stage2").gameObject.SetActive(false);
                 break;
             case 1:
                 transform.Find("Stage0").gameObject.SetActive(false);
                 transform.Find("Stage1").gameObject.SetActive(true);
+                transform.Find("Stage2").gameObject.SetActive(false);
+                break;
+            case 2:
+                transform.Find("Stage0").gameObject.SetActive(false);
+                transform.Find("Stage1").gameObject.SetActive(false);
+                transform.Find("Stage2").gameObject.SetActive(true);
                 break;
         }   
 
@@ -103,9 +133,15 @@ public class StageSelectController : MonoBehaviour
         return record[i, 3];
     }
 
+    public static bool doStage(int n)
+    {
+        if (record[n - 1, 1] == 1) return true;
+        else return false;
+    }
+
     public void TabRightArrow()
     {
-        if (stageNumber < 1)
+        if (stageNumber < 2)
             stageNumber++;
     }
 
