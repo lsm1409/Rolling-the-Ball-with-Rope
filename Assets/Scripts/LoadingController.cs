603e6a571d6fe757ec2a59262a1e8b24340bc3ca
Yoo Jinhyeok <yujh703@gmail.com>
2019-10-24 23:06:12 +0300

    로딩스크립트 원복 & 0번 맵 오류수정

--- Assets/Scripts/LoadingController.cs
+++ Assets/Scripts/LoadingController.cs
@@ -60,6 +60,7 @@ public class LoadingController : MonoBehaviour
     public static string nextScene;
 
     [SerializeField]
+    Image progressBar;
 
     private void Start()
     {
@@ -81,11 +82,27 @@ public class LoadingController : MonoBehaviour
         op.allowSceneActivation = false;
 
         float timer = 0.0f;
-        while (!op.isDone)
-        {
-            yield return null;
+        //while (!op.isDone)
+        //{
+        //    yield return null;
 
-            timer += Time.deltaTime;
-        }
+        //    timer += Time.deltaTime;
+
+        //    if (op.progress >= 0.9f)
+        //    {
+        //        progressBar.fillAmount = Mathf.Lerp(progressBar.fillAmount, 1f, timer);
+
+        //        if (progressBar.fillAmount == 1.0f)
+        //            op.allowSceneActivation = true;
+        //    }
+        //    else
+        //    {
+        //        progressBar.fillAmount = Mathf.Lerp(progressBar.fillAmount, op.progress, timer);
+        //        if (progressBar.fillAmount >= op.progress)
+        //        {
+        //            timer = 0f;
+        //        }
+        //    }
+        //}
     }
 }
