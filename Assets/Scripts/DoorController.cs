189e27424b57ea64a6f344cd2d75610521ab3e1a
Sangmin <38128568+lsm1409@users.noreply.github.com>
2019-10-29 12:56:45 +0900

    회전문 회전

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -1,11 +1,11 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class DoorController : MonoBehaviour
 {
+    public int doorNum;
     public int switchNum;
     public float rotationAngle;
-
-    private GameDirector gameDirector;
 
     // Start is called before the first frame update
     void Start()
@@ -16,9 +16,21 @@ public class DoorController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (true)
+        if (GameDirector.switches[switchNum] && !GameDirector.doors[doorNum])
         {
+            StartCoroutine(Rotate(Vector3.forward * 90, 2.0f));
+            GameDirector.doors[doorNum] = true;
+        }
+    }
 
+    IEnumerator Rotate(Vector3 byAngles, float inTime)
+    {
+        var fromAngle = transform.rotation;
+        var toAngle = Quaternion.Euler(transform.eulerAngles + byAngles);
+        for (var t = 0f; t < 1; t += Time.deltaTime / inTime)
+        {
+            transform.rotation = Quaternion.Slerp(fromAngle, toAngle, t);
+            yield return null;
         }
     }
 }
--- Assets/Scripts/GameDirector.cs
+++ Assets/Scripts/GameDirector.cs
@@ -11,6 +11,7 @@ public class GameDirector : MonoBehaviour
     public static int RespawnPoint;     // 리스폰
     public static bool isPaused;        // 게임 일시정지
     public static bool[] switches = new bool[3];
+    public static bool[] doors = new bool[3];
 
     // Start is called before the first frame update
     void Start()
