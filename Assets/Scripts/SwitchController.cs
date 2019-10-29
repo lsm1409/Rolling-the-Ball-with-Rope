6d4c048b69fa53701957ffb543178784c48c0e2b
carlyzzz <carlyzzz@naver.com>
2019-10-29 12:38:57 +0900

    스위치 컨트롤러
    만듬

---
+++ Assets/Scripts/SwitchController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchController : MonoBehaviour
+{
+    private bool isClicked;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isClicked = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public bool getClick()
+    {
+        return isClicked;
+    }
+    public void setClick(bool a)
+    {
+        isClicked = a;
+    }
+}
